8639a289967acafad83312ba5249f47e0d014766
pedroa.desenvolvimento@outlook.com <pedroa.desenvolvimento@outlook.com>
2022-09-30 11:34:46 -0300

    adicionado copyToClipboard

--- src/components/Calc/index.jsx
+++ src/components/Calc/index.jsx
@@ -3,12 +3,22 @@ import { useState } from "react"
 
 export default function Calc(){
     const [valor, setValor] = useState(0);
+    const [styleCpy, setStyleCpy] = useState(styles.dNone);
     
     
     const onChangeHandler = event => {
         setValor(event.target.value);
      };
+    
+    function copyToClipdoard(){
+        navigator.clipboard.writeText(valor/16+"rem");
+        setStyleCpy(styles.copy);
 
+        setTimeout(() => {
+            setStyleCpy(styles.dNone);
+
+          }, 3000);
+    }
     
     return(
 
@@ -18,7 +28,8 @@ export default function Calc(){
             <h3>Digite o valor (px)</h3>
             <input type="text" value={valor} onChange={onChangeHandler}/>
 
-            <h1 onClick={() => {navigator.clipboard.writeText(valor/16+"rem")}}>{valor / 16 }rem</h1>
+            <h1 onClick={copyToClipdoard}>{valor / 16 }rem</h1>
+            <div className={styleCpy}>Copiado!</div>
 
            </div>
         </>
